820289ba818b082edc12f332494157d490fa87d1
Tiago Temporin <tiago@rocketcommerce.com.br>
2015-05-16 08:27:36 -0300

    add mapping object

--- es/mapping.go
+++ es/mapping.go
@@ -2,5 +2,70 @@ package es
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/tiaguinho/esmsync/mongo"
 	"io/ioutil"
+	"reflect"
+	"strings"
 )
+
+//Mapping struct
+type Node struct {
+	MongoField string `json:"mongo"`
+	Type       string `json:"type"`
+	EsField    string `json:"es"`
+}
+
+func getNodesFile() []Node {
+	var nodes []Node
+
+	content, err := ioutil.ReadFile("./config/mapping.json")
+	if err == nil {
+		json.Unmarshal(content, &nodes)
+	}
+
+	return nodes
+}
+
+//map a struct to the model defined in mapping.json
+func Mapping(oplog interface{}) {
+	nodes := getNodesFile()
+
+	var data map[string]interface{}
+	switch reflect.ValueOf(oplog).Field(0).FieldByName("Op").String() {
+	case "i":
+		s := oplog.(mongo.OplogInsert)
+		data = s.O
+	case "u":
+		s := oplog.(mongo.OplogUpdate)
+		data = s.O
+		//case "d":
+		//s := oplog.(mongo.OplogDelete)
+	}
+
+	if len(data) != 0 {
+		for _, node := range nodes {
+			getValue(node.MongoField, data)
+		}
+	} else {
+		//TODO
+	}
+}
+
+//return a value of the field
+func getValue(key string, data map[string]interface{}) {
+	if data[key] == nil {
+		fields := strings.Split(key, ">")
+
+		for _, field := range fields {
+			if reflect.TypeOf(data).Kind() == reflect.Map {
+				resp = data[field]
+				//TODO
+			} else {
+				fmt.Println(key, "=", data)
+			}
+		}
+	} else {
+		fmt.Println(key, "=", data[key])
+	}
+}
--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/tiaguinho/esmsync/es"
 	"github.com/tiaguinho/esmsync/mongo"
 	"io/ioutil"
+	"os"
+	"reflect"
 )
 
 func main() {
@@ -20,18 +21,29 @@ func main() {
 
 			inserts := mongo.GetOplogsInsert(session, conf.Db, conf.C)
 			if len(inserts) > 0 {
-
+				sync(inserts)
 			}
 
 			updates := mongo.GetOplogsUpdate(session, conf.Db, conf.C)
 			if len(updates) > 0 {
-				//TODO
+				sync(updates)
 			}
 
 			deletes := mongo.GetOplogsDelete(session, conf.Db, conf.C)
 			if len(deletes) > 0 {
-				//TODO
+				sync(deletes)
 			}
 		}
+	}
+}
+
+//sync data between mongo and elasticsearch
+func sync(oplogs interface{}) {
+	length := reflect.ValueOf(oplogs).Len()
+
+	for i := 1; i < length; i++ {
+		es.Mapping(reflect.ValueOf(oplogs).Index(i).Interface())
+
+		os.Exit(400)
 	}
 }
